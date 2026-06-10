bc0481806acdb787332536f3236ddabc8f1707bc
francearena-arch <france.arena@googlemail.com>
2026-06-10 22:57:07 +0200

    BodyPlan v13
    Navigation redesign and streamlined dashboard

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-BodyPlan PWA v12
-
-Bereinigte Codebasis (doppelte Funktionen entfernt) plus Daten-Export/-Import
-über das Burger-Menü oben rechts. Backup als JSON sichern und bei Bedarf
-wiederherstellen.
+const CACHE_NAME='bodyplan-v13';
+const ASSETS = ['./','./index.html','./manifest.json','./service-worker.js','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))); });
