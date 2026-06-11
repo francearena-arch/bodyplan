de6b01446dbdf64b76b3690a12478266818cd0cf
francearena-arch <france.arena@googlemail.com>
2026-06-11 22:17:09 +0200

    BodyPlan v18: Add daily consistency tracking

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
-BodyPlan PWA v17
-
-Dashboard refinement, home navigation, milestone card and static training history page.
+const CACHE_NAME='bodyplan-v18';
+const ASSETS = ['./','./index.html','./manifest.json','./service-worker.js','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))); });
