6e75558b5d82ea27ab47da19faf3aa89fd341aa2
francearena-arch <france.arena@googlemail.com>
2026-06-13 15:43:43 +0200

    BodyPlan v29
    Remove redundant workout session header

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='bodyplan-v28';
+const CACHE_NAME='bodyplan-v29';
 const ASSETS = ['./','./index.html','./manifest.json','./service-worker.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
