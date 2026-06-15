8a52afa3cb55252596e0d6e0b47e980570f77ef8
francearena-arch <france.arena@googlemail.com>
2026-06-16 00:21:57 +0200

    BodyPlan v38: Rebuild heatmap as dynamic SVG

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,17 @@
-const CACHE_NAME='bodyplan-v36';
-const ASSETS = ['./','./index.html?v=36','./manifest.json?v=36','./service-worker.js?v=36','./icons/icon-v36-192.png?v=36','./icons/icon-v36-512.png?v=36','./icons/icon-192.png?v=36','./icons/icon-512.png?v=36','./apple-touch-icon-v36.png?v=36','./apple-touch-icon.png?v=36','./favicon.png?v=36'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request,{cache:'no-store'}).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))); });
+const CACHE_NAME='bodyplan-v38';
+const ASSETS=[
+'./',
+'./index.html?v=38',
+'./manifest.json?v=38',
+'./service-worker.js?v=38',
+'./icons/icon-v38-192.png?v=38',
+'./icons/icon-v38-512.png?v=38',
+'./icons/icon-192.png?v=38',
+'./icons/icon-512.png?v=38',
+'./apple-touch-icon-v38.png?v=38',
+'./apple-touch-icon.png?v=38',
+'./favicon.png?v=38'
+];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));});
