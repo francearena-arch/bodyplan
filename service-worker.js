693c5846f5b7349eecf453c056215ec4d8fa3676
francearena-arch <france.arena@googlemail.com>
2026-06-15 23:24:50 +0200

    BodyPlan v35
    Fix all icon and image asset references

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='bodyplan-v33';
-const ASSETS = ['./','./index.html','./manifest.json?v=33','./service-worker.js','./icons/icon-v32-192.png','./icons/icon-v32-512.png','./assets/body-heatmap-front-v33.png','./assets/body-heatmap-back-v33.png'];
+const CACHE_NAME='bodyplan-v35';
+const ASSETS = ['./','./index.html?v=35','./manifest.json?v=35','./service-worker.js?v=35','./icons/icon-v35-192.png?v=35','./icons/icon-v35-512.png?v=35','./assets/body-heatmap-front-v35.png?v=35','./assets/body-heatmap-back-v35.png?v=35','./icons/icon-192.png?v=35','./icons/icon-512.png?v=35','./apple-touch-icon.png?v=35','./favicon.png?v=35'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))); });
